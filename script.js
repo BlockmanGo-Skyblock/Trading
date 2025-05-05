d0d54d5d89f9d4ecb1f63f1d4d8aaea7cd19ca7a
BlockmanGo-Skyblock <bloxfruitsisbest55@gmail.com>
2025-05-05 18:50:56 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("login-form")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Login logic coming soon!");
+});
+
+document.getElementById("signup-form")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Signup logic coming soon!");
+});
