1fbc794cc3b21012e0c563978247e44138af660f
BlockmanGo-Skyblock <bloxfruitsisbest55@gmail.com>
2025-05-05 19:39:27 +0200

    home.js

---
+++ home.js
@@ -0,0 +1,25 @@
+// home.js
+import { auth } from './firebase.js';
+import {
+  getFirestore, doc, getDoc
+} from 'https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js';
+
+const db = getFirestore();
+
+auth.onAuthStateChanged(async (user) => {
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const docRef = doc(db, "users", user.uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    document.getElementById("display-name").textContent = data.username;
+    document.getElementById("profile-pic").src = data.profilePic;
+  } else {
+    alert("User profile not found.");
+  }
+});
