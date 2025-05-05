fd52dfd9f616c6e819f920c8c95bad6820c4d1c8
BlockmanGo-Skyblock <bloxfruitsisbest55@gmail.com>
2025-05-05 19:00:26 +0200

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,16 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "123456",
+  appId: "1:123456:web:abc123"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
