b898cf1529257c0a77d3836b8d8d28b086f44f7f
BlockmanGo-Skyblock <bloxfruitsisbest55@gmail.com>
2025-05-05 19:52:53 +0200

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAdn3zYN3w2I7Gahihrkg2f3kK1oPKOMwg",
   authDomain: "bmgo-skyblock-trading.firebaseapp.com",
   projectId: "bmgo-skyblock-trading",
-  storageBucket: "bmgo-skyblock-trading.firebasestorage.app",
+  storageBucket: "bmgo-skyblock-trading.appspot.com",
   messagingSenderId: "664816402677",
   appId: "1:664816402677:web:d5d6b7c4b60c5679fd05bc",
   measurementId: "G-0YM4VQZN8W"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth };
